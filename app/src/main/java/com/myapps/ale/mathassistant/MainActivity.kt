67455c0c514426d84625ad51c835eb8f40a4c62d
blaze124 <blazedecai@hotmail.com>
2018-03-26 23:30:57 +0200

    Current status: GUI at 100%

--- app/src/main/java/com/myapps/ale/mathassistant/MainActivity.kt
+++ app/src/main/java/com/myapps/ale/mathassistant/MainActivity.kt
@@ -2,11 +2,16 @@ package com.myapps.ale.mathassistant
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spnOperation.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.spnOppElements))
+
     }
 }
