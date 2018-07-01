35749163c4e125de39d939d68b07df4af1e1c5e7
blaze124 <blazedecai@hotmail.com>
2018-07-01 22:02:13 +0200

    2018/07/01- Fin desarrollo funcionalidad básica

--- app/src/main/java/com/myapps/ale/mathassistant/MainActivity.kt
+++ app/src/main/java/com/myapps/ale/mathassistant/MainActivity.kt
@@ -3,6 +3,8 @@ package com.myapps.ale.mathassistant
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,40 @@ class MainActivity : AppCompatActivity() {
 
         spnOperation.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.spnOppElements))
 
+        btnCalculate.setOnClickListener() {
+            calculate()
+        }
+    }
+
+    fun calculate () {
+        var op1 = txtOp1.text.toString().toFloatOrNull()
+        var op2 = txtOp2.text.toString().toFloatOrNull()
+        var res = txtRes.text.toString().toFloatOrNull()
+
+        var opValue = resources.getStringArray(R.array.spnOppValues)[spnOperation.selectedItemPosition]
+
+        if(op1 == null)
+            op1 = Float.MIN_VALUE
+
+        if(op2 == null)
+            op2 = Float.MIN_VALUE
+
+        if(res == null)
+            res = Float.MIN_VALUE
+
+        var isOk = resultCheck(op1 , op2, res, opValue)
+
+        Toast.makeText(this, if (isOk) "OK" else "NOK", Toast.LENGTH_SHORT).show()
+    }
+
+    fun resultCheck(op1: Float, op2: Float, op3: Float, ope: String): Boolean {
+        var res = false
+        when (ope){
+            "+" -> res = (op1 + op2 == op3)
+            "-" -> res = (op1 - op2 == op3)
+            "*" -> res = (op1 * op2 == op3)
+            "/" -> res = (op1 / op2 == op3)
+        }
+        return res
     }
 }
